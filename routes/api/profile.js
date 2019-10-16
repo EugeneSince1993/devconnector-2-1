a14c2f3fd861a8cd90d9cf8924985ce6d81cc6de
EugeneSince1993 <jk235@mail.ru>
2019-10-16 21:20:19 +0300

    Step 20. Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -187,5 +187,60 @@ router.delete(
   }
 );
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience', 
+  [ auth, 
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { 
+      title, 
+      company, 
+      location, 
+      from, 
+      to, 
+      current, 
+      description } = req.body;
+
+      // New Experience
+      const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+      }
+
+      // Dealing with MongoDB
+      try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        /* unshift() adds items to the array like push() method. the difference is that unshift() adds items to the beginning of the array. and push() adds items to the end of the array*/
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+  }
+);
+
 
 module.exports = router;
