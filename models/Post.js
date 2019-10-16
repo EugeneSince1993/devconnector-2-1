50c2ec2a270356614fd7de526b10dd2b7e47c28c
EugeneSince1993 <jk235@mail.ru>
2019-10-17 00:51:22 +0300

    Step 24. Creating the Post model

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  /* name of the user - so that if the user will be deleted,
+     we would be able not to delete his posts. */
+  name: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  /* "likes" will be an array of users objects that have the id,
+      which is an "ObjectId" */
+  likes: [ 
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
