7c684b960f25cb3ee4b9d397a643d4dd0e837fb0
kis92 <kis795265@gmail.com>
2019-10-18 04:35:02 +0300

    Step 28. Add & remove comment routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -34,6 +34,7 @@ router.post(
       so we'll use "select('-password')" */
       const user = await User.findById(req.user.id).select('-password');
 
+      // there is a Post collection in the database
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
@@ -62,8 +63,10 @@ router.get(
   auth,
   async (req, res) => {
     try {
-      // to get all items, we use "find()", for single one - "findOne"
-      /* parameter of { date: -1 } sort the post from the most recent one.
+      /* to get the value of the first element in the array, 
+         we use "find()" (JS method), for single one - "findOne" (Mongoose
+         method),
+         parameter of { date: -1 } sort the post from the most recent one.
          { date: 1 } is to sort from the oldest one, but that's the default setting.  */
       const posts = await Post.find().sort({ date: -1 });
       res.json(posts);
@@ -76,14 +79,14 @@ router.get(
 
 // @route   GET api/posts/:id
 // @desc    Get post by (its) ID
-/* @access  Private */
+/* @access  Private - we should be authorized */
 router.get(
   '/:id',
   auth,
   async (req, res) => {
     try {
        /* Get the post by its ID.
-          we insert "await" because mongoose methods like find(),
+          we insert "await" because mongoose methods like
           findOne() of findById() return a promise.
           "req.params.id" will allow us to get id from the URL. */ 
       const post = await Post.findById(req.params.id);
@@ -110,7 +113,7 @@ router.get(
 
 // @route   DELETE api/posts/:id
 // @desc    Delete a post (by its ID)
-// @access  Private
+// @access  Private - we should be authorized
 router.delete(
   '/:id',
   auth,
@@ -159,7 +162,7 @@ like (at the first time), our route should add this like to the array.
 Pressing like or unlike, we're updating "likes" array in the Post model.
 We technically update the Post. */ 
 // @desc    Like a post 
-// @access  Private
+// @access  Private - we should be authorized
 router.put(
   '/like/:id',
   auth,
@@ -210,7 +213,7 @@ router.put(
 
 // @route   PUT api/posts/unlike/:id
 // @desc    Unlike a post (remove a like)
-// @access  Private
+// @access  Private - we should be authorized
 router.put(
   '/unlike/:id',
   auth,
@@ -249,6 +252,115 @@ router.put(
         await post.save();
         
         res.json(post.likes);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+/* @route   POST api/posts/comment/:id - we need the
+post id to add a comment on. */ 
+// @desc    Comment on a post
+// @access  Private - we should be authorized
+router.post(
+  '/comment/:id',
+  [
+    // Get, check, verify token
+    auth,
+    // Validation
+    [
+      check('text', 'Text is required')
+      .not()
+      .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // errors is an object, array() is its method
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      // get the user by id - we have user id in the token
+      const user = await User.findById(req.user.id).select('-password');
+      // get the post - we have it in the URL
+      const post = await Post.findById(req.params.id);
+
+      // Create comment
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      }
+
+      // Add the new comment on to "post.comments" array
+      // add on the beginning of the array
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+/* @route   DELETE api/posts/comment/:id/:comment_id - because
+we need both post id and comment id. We need to find the post
+by the id and then we need to know which comment to delete.  */
+// @desc    Delete comment
+// @access  Private - we should be authorized
+router.delete(
+  '/comment/:id/:comment_id',
+  auth,
+  async (req, res) => {
+    try {
+      // get the post by its id which is in the URL
+      const post = await Post.findById(req.params.id);
+
+      // pull out (get) the comment from the post
+      /* we wanna find a comment by its id.
+         find() method takes in functions like
+         forEach(), map(), filter(). find() kinda
+         copy their functionality.
+         We wanna test to see if the comment.id
+         is equal to the comment_id. This will give us
+         either comment (if it exists) or "false". */
+      const comment = post.comments
+      .find(comment => comment.id === req.params.comment_id);
+      // Make sure comment exists
+      if (!comment) {
+        // Status 404 - Not Found
+        return res.status(404).json({ msg: 'Comment does not exist' });
+      }
+
+      /* Check user - we need to make sure that the user deleting comment
+         actually made it.
+         The comment (object) has an "user" property that is shown as
+         an user id. "comment.user" is a user who made comment, "req.user.id" is a logged-in user deleting this comment.
+         "comment.user" is an ObjectId, so we'll turn it to a string. */
+      if (comment.user.toString() !== req.user.id) {
+        // Status 401 - Not Authorized (Unauthorized)
+        return res.status(401).json({ msg: 'User not authorized' });
+      }
+
+      // Get remove index - to remove the found comment
+      const removeIndex = post.comments
+      .map(comment => comment.user.toString()).indexOf(req.user.id);
+
+      /*  Take out the comment from the "comments" array
+          with "splice()" method.
+          the second parameter of "1" means that
+          we just wanna remove 1 from that. */
+      post.comments.splice(removeIndex, 1);
+
+      await post.save();
+
+      res.json(post.comments);
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
