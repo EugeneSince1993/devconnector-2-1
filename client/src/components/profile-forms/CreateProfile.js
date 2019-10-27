cb0c0bc8e36cda04a1f3a8ce671c619ecc9ef042
EugeneSince1993 <jk235@mail.ru>
2019-10-27 20:42:30 +0300

    Step 47. CreateProfile Component.

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,237 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const CreateProfile = () => {
+  /* In here we'll put the default values in the fields.
+  That's the "formData" state with the default values.
+  "setFormData" is a method to update the state. */
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
+  });
+
+  /* "displaySocialInputs" is an another piece of state. It stores the group of the social media inputs. "toggleSocialInputs" is a method to update the "displaySocialInputs" state (to toggle - hide or show). The social inputs should be hidden by default. Default value will be "false" (so it will be a boolean value). */
+  const [displaySocialInputs, toggleSocialInputs] = useState(false);
+
+  /* Destructure all the props from the state. We're gonna pull all these fields from the formData. */
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = formData;
+
+  const onChange = e => setFormData({
+    /* The rest of the "formData" */
+    ...formData,
+    /* For the particular field, we're gonna get it by the name, so we're gonna do "e.target.name" - we're gonna use that as the key. And then the value - "e.target.value".
+    So that will change it (the field value) in the state. */
+    [e.target.name]: e.target.value
+  });
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">
+        Create Your Profile
+      </h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> 
+        Let's get some information to make your profile stand out
+      </p>
+      <small>* = required fields</small>
+      <form className="form">
+        <div className="form-group">
+          {/* All of our social inputs will have "onChange" event handler. */}
+          <select 
+            name="status" 
+            value={status} 
+            /* We just call an "onChange()" event handler and pass in an event. */
+            onChange={e => onChange(e)} 
+          >
+            <option value="0">* Select Professional Status</option>
+            <option value="Developer">Developer</option>
+            <option value="Junior Developer">Junior Developer</option>
+            <option value="Senior Developer">Senior Developer</option>
+            <option value="Manager">Manager</option>
+            <option value="Student or Learning">Student or Learning</option>
+            <option value="Instructor">Instructor or Teacher</option>
+            <option value="Intern">Intern</option>
+            <option value="Other">Other</option>
+          </select>
+          <small className="form-text">
+            Give us an idea of where you are at in your career
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Company" 
+            name="company" 
+            value={company} 
+            onChange={e => onChange(e)} 
+          />
+          <small className="form-text">
+            Could be your own company or one you work for
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Website" 
+            name="website" 
+            value={website} 
+            onChange={e => onChange(e)} 
+          />
+          <small className="form-text">
+            Could be your own or a company website
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Location" 
+            name="location" 
+            value={location} 
+            onChange={e => onChange(e)} 
+          />
+          <small className="form-text">
+            City & state suggested (e.g., Boston, MA)
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="* Skills" 
+            name="skills" 
+            value={skills} 
+            onChange={e => onChange(e)} 
+          />
+          <small className="form-text">
+            Please use comma separated values (e.g., HTML, CSS, JavaScript, PHP)
+          </small>
+        </div>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="GitHub Username" 
+            name="githubusername" 
+            value={githubusername} 
+            onChange={e => onChange(e)} 
+          />
+          <small className="form-text">
+            If you want your latest repos and a GitHub Link, include your username
+          </small>
+        </div>
+        <div className="form-group">
+          <textarea 
+            placeholder="A short bio of yourself" 
+            name="bio"
+            value={bio} 
+            onChange={e => onChange(e)} 
+          ></textarea>
+          <small className="form-text">
+            Tell us a little about yourself
+          </small>
+        </div>
+        <div className="my-2">
+          {/* 
+            Let's add an "onClick" event handler to this button to toggle the "displaySocialInputs" state (from "true" to "false") by the "toggleSocialInputs()" method. And we want it to be whatever the opposite. If it's true, we wanna set to false and vice versa.
+          */}
+          <button 
+            onClick={() => toggleSocialInputs(!displaySocialInputs)} 
+            type="button" 
+            className="btn btn-light"
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+        {/* 
+          We wanna test for "displaySocialInputs". If that's true then we're gonna show a Fragment, and we're gonna have inside this Fragment all these divs with the social inputs (form-groups). So that way these (social inputs) will only display if this "displaySocialInputs" (state) value is "true".  
+        */}
+        {
+          displaySocialInputs &&
+            <Fragment>
+              <div className="form-group social-input">
+                <i className="fab fa-twitter fa-2x"></i>
+                <input 
+                  type="text" 
+                  placeholder="Twitter URL" 
+                  name="twitter" 
+                  value={twitter} 
+                  onChange={e => onChange(e)} 
+                />
+              </div>
+              <div className="form-group social-input">
+                <i className="fab fa-facebook fa-2x"></i>
+                <input 
+                  type="text" 
+                  placeholder="Facebook URL" 
+                  name="facebook" 
+                  value={facebook} 
+                  onChange={e => onChange(e)} 
+                />
+              </div>
+              <div className="form-group social-input">
+                <i className="fab fa-youtube fa-2x"></i>
+                <input 
+                  type="text" 
+                  placeholder="Youtube URL" 
+                  name="youtube" 
+                  value={youtube} 
+                  onChange={e => onChange(e)} 
+                />
+              </div>
+              <div className="form-group social-input">
+                <i className="fab fa-linkedin fa-2x"></i>
+                <input 
+                  type="text" 
+                  placeholder="LinkedIn URL" 
+                  name="linkedin" 
+                  value={linkedin} 
+                  onChange={e => onChange(e)}
+                />
+              </div>
+              <div className="form-group social-input">
+                <i className="fab fa-instagram fa-2x"></i>
+                <input 
+                  type="text" 
+                  placeholder="Instagram URL" 
+                  name="instagram" 
+                  value={instagram} 
+                  onChange={e => onChange(e)} 
+                />
+              </div>
+            </Fragment>
+        }
+        <input type="submit" value="Submit" className="btn btn-primary my-1" />
+        <a className="btn my-1" href="dashboard.html">Go Back</a>
+      </form>
+    </Fragment>
+  );
+};
+
+CreateProfile.propTypes = {
+
+};
+
+export default CreateProfile;
